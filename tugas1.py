7254f5379a3f59bfdfecb84ab98e0109810b9b18
wirapratidinaiptutahangpalle <97576559+wira2001@users.noreply.github.com>
2022-03-15 10:25:54 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,54 @@
+print ("")
+print ("silahkan pilih bilangan di bawah ini")
+print ("---------------")
+print ("1.penjumlahan")
+print ("2.pengurangan")
+print ("3.perkalian")
+print ("4.pembagian")
+print ("===============")
+
+bilangan = int (input ("Masukkan pilihan bilangan 1/2/3/4   :"))
+bil1     = int (input ("Masukkan bilangan pertama           :"))
+bil2     = int (input ("Masukkan bilangan kedua             :"))
+
+if (bilangan==1 ):
+    print  (bil1," + " ,bil2," = ",(bil1 + bil2))
+
+elif(bilangan==2):
+    print  (bil1," - " ,bil2," = ",(bil1 - bil2))
+    
+elif(bilangan==3):
+    print  (bil1," x " ,bil2," = ",(bil1 * bil2))
+    
+elif(bilangan==4):
+    print  (bil1," / " ,bil2," = ",(bil1 / bil2))
+    
+else:
+    print ("tidak ada pilihan")
+print ("")
+print ("silahkan pilih bilangan di bawah ini")
+print ("---------------")
+print ("1.penjumlahan")
+print ("2.pengurangan")
+print ("3.perkalian")
+print ("4.pembagian")
+print ("===============")
+
+bilangan = int (input ("Masukkan pilihan bilangan 1/2/3/4   :"))
+bil1     = int (input ("Masukkan bilangan pertama           :"))
+bil2     = int (input ("Masukkan bilangan kedua             :"))
+
+if (bilangan==1 ):
+    print  (bil1," + " ,bil2," = ",(bil1 + bil2))
+
+elif(bilangan==2):
+    print  (bil1," - " ,bil2," = ",(bil1 - bil2))
+    
+elif(bilangan==3):
+    print  (bil1," x " ,bil2," = ",(bil1 * bil2))
+    
+elif(bilangan==4):
+    print  (bil1," / " ,bil2," = ",(bil1 / bil2))
+    
+else:
+    print ("tidak ada pilihan")
